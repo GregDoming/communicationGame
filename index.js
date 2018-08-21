8781ae2af7b8cad1fd316f1dd20a6757121cec29
GregDoming <greg.domingue1@gmail.com>
2018-08-21 12:09:35 -0700

    my first solo commit, added depencies to referenced in txt steps

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config
+
+const express = require('express');
+const react = require('react');
+const redux = require('redux');
+const reactredux = require('react-redux');
+const mongoose = require('mongoose');
+const mongodb = require('mongodb');
+const bodyParser = require('body-parser');
+
